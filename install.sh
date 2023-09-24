66d6f216af320af5d6ee2cbd0d3d76525dd51da1
TohurTV <joshwebb84@outlook.com>
2023-09-24 02:28:58 -0600

    fixes to the install script and added new screenshots

--- install.sh
+++ install.sh
@@ -5,12 +5,6 @@ INSTALL_DIR="/opt/llama.cpp-qt"
 BIN_DIR="/usr/bin"
 DESKTOP_DIR="/usr/share/applications"
 
-# Setup proper python venv
-python3 -m venv --system-site-packages "venv"
-source "./venv/bin/activate"
-pip install -r requirements.txt
-deactivate
-
 # Ask for the sudo password
 echo "Please enter your sudo password:"
 sudo -v
@@ -18,7 +12,6 @@ sudo -v
 # Check if llama.cpp-qt.py exists in /opt/llama.cpp-qt
 if [ -f "/opt/llama.cpp-qt/llama.cpp-qt.py" ]; then
     sudo rm -r -f -d /opt/llama.cpp-qt
-    exit $?
 fi
 
 # Create the installation directory
@@ -34,7 +27,6 @@ sudo cp llama.cpp-qt.py "$INSTALL_DIR"
 sudo cp llama.png "$INSTALL_DIR"
 sudo cp requirements.txt "$INSTALL_DIR"
 sudo cp -r public "$INSTALL_DIR/public"
-sudo cp -r venv "$INSTALL_DIR/venv"
 sudo cp llama.cpp-qt "$BIN_DIR"
 
 # Set proper permissions
@@ -42,6 +34,13 @@ sudo chmod 755 "$INSTALL_DIR/api_like_OAI.py"
 sudo chmod 755 "$INSTALL_DIR/server"
 sudo chmod 755 "$INSTALL_DIR/llama.cpp-qt.py"
 sudo chmod 755 "$BIN_DIR/llama.cpp-qt"
+
+# Setup proper python venv
+cd $INSTALL_DIR
+sudo python3 -m venv --system-site-packages "venv"
+source "./venv/bin/activate"
+pip install -r requirements.txt
+deactivate
 
 # Create a desktop shortcut for llama.cpp-qt
 echo "Creating desktop shortcut..."
