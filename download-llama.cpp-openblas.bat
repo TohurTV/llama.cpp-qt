85ef31272fe84c5b864198a67675a4f7146c93d2
TohurTV <joshwebb84@outlook.com>
2023-09-28 09:29:30 -0600

    llama.cpp downloader fixes

--- download-llama.cpp-openblas.bat
+++ download-llama.cpp-openblas.bat
@@ -9,13 +9,24 @@ set TAG=b1280
 set TEMP_DIR=%TEMP%\llama_cpp_temp
 mkdir "%TEMP_DIR%"
 
-:: Download the latest release assets
-echo Downloading latest release...
-curl -LJO https://github.com/%REPO%/releases/%TAG%/download/llama-b1280-bin-win-openblas-x64.zip
+:: Direct download link to the zip file
+set "download_url=https://github.com/%REPO%/releases/download/%TAG%/llama-%TAG%-bin-win-openblas-x64.zip"
+
+:: Create a temporary PowerShell script
+echo Invoke-WebRequest -Uri "!download_url!" -OutFile llama-%TAG%-bin-win-openblas-x64.zip > download.ps1
+
+:: Download the zip file using PowerShell
+powershell.exe -ExecutionPolicy Bypass -File download.ps1
+
+:: Check if the downloaded file is valid
+if not exist llama-%TAG%-bin-win-openblas-x64.zip (
+  echo Error: Failed to download the zip file.
+  exit /b 1
+)
 
 :: Extract the downloaded release zip file using Windows built-in zip utility
 echo Extracting files...
-Call :UnZipFile %TEMP_DIR% llama-b1280-bin-win-openblas-x64.zip
+powershell -command "Expand-Archive -Path 'llama-%TAG%-bin-win-openblas-x64.zip' -DestinationPath '%TEMP_DIR%'"
 
 :: Copy DLL files and server.exe to the current directory
 echo Copying files to the current directory...
@@ -26,7 +37,7 @@ copy "%TEMP_DIR%\server.exe" .
 :: Cleanup: Remove the temporary directory and downloaded zip file
 echo Cleaning up...
 rd /s /q "%TEMP_DIR%"
-del llama-b1280-bin-win-openblas-x64.zip
+del llama-%TAG%-bin-win-openblas-x64.zip
 
 :: Done
 echo Latest release downloaded and extracted successfully.
